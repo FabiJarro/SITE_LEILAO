b0a40939c4b1bf6d23106f6d697119c50d87d118
FabiJarro <fabii.jarro@gmail.com>
2025-11-27 02:50:11 -0300

    ultimoultimo

--- leilao.py
+++ leilao.py
@@ -23,7 +23,7 @@ from views_deletarEditar import *
 from views_paginasStatic import *
 
 if __name__ == '__main__':
-    #app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5001)
     app.run(debug=True)
 
 
